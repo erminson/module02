4a67c196177fa6339e6d6d27ca4137a3d066e6f2
Lev Erminson <erminson@gmail.com>
2021-10-08 17:32:24 +0300

    Refactoring RecordBookRepositoryImpl: remove loading data from file and add parameterized constructor

--- learning-center-impl/src/main/java/ru/erminson/ec/impl/repository/RecordBookRepositoryImpl.java
+++ learning-center-impl/src/main/java/ru/erminson/ec/impl/repository/RecordBookRepositoryImpl.java
@@ -1,27 +1,21 @@
 package ru.erminson.ec.impl.repository;
 
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
-import ru.erminson.ec.impl.Main;
-import ru.erminson.ec.model.dto.yaml.YamlRecordBookList;
 import ru.erminson.ec.model.entity.RecordBook;
 import ru.erminson.ec.model.entity.Student;
 import ru.erminson.ec.api.repository.RecordBookRepository;
-import ru.erminson.ec.api.repository.StudentRepository;
-import ru.erminson.ec.impl.utils.RecordBookInitializer;
 
-import java.io.InputStream;
 import java.util.*;
 
 public class RecordBookRepositoryImpl implements RecordBookRepository {
-    private static final String RECORD_BOOKS_FILE_NAME = "recordbooks.yaml";
 
-    private final Map<Student, RecordBook> storage = new HashMap<>();
-    private StudentRepository studentRepository;
+    private final Map<Student, RecordBook> storage;
 
-    public RecordBookRepositoryImpl(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-        init();
+    public RecordBookRepositoryImpl() {
+        this.storage = new HashMap<>();
+    }
+
+    public RecordBookRepositoryImpl(Map<Student, RecordBook> storage) {
+        this.storage = storage;
     }
 
     @Override
@@ -49,26 +43,5 @@ public class RecordBookRepositoryImpl implements RecordBookRepository {
     @Override
     public List<Student> getAllStudents() {
         return new ArrayList<>(storage.keySet());
-    }
-
-    private void init() {
-        Yaml yaml = new Yaml(new Constructor(YamlRecordBookList.class));
-        InputStream inputStream = Main
-                .class
-                .getClassLoader()
-                .getResourceAsStream(RECORD_BOOKS_FILE_NAME);
-
-        YamlRecordBookList list = yaml.loadAs(inputStream, YamlRecordBookList.class);
-        list.getRecordBooks().stream()
-                        .forEach(recordBookDto -> {
-                            String studentName = recordBookDto.getStudentName();
-                            try {
-                                Student student = studentRepository.getStudentByName(studentName);
-                                RecordBook recordBook = RecordBookInitializer.createRecordBookByCourse(recordBookDto);
-                                storage.put(student, recordBook);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        });
     }
 }
