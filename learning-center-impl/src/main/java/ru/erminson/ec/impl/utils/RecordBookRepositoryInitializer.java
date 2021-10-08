2ce1eca940826d51d5090a30c68cedc23035cfab
Lev Erminson <erminson@gmail.com>
2021-10-08 17:33:14 +0300

    Add RecordBookRepositoryInitializer for init repository from ymal file

---
+++ learning-center-impl/src/main/java/ru/erminson/ec/impl/utils/RecordBookRepositoryInitializer.java
@@ -0,0 +1,56 @@
+package ru.erminson.ec.impl.utils;
+
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import ru.erminson.ec.api.repository.RecordBookRepository;
+import ru.erminson.ec.api.repository.StudentRepository;
+import ru.erminson.ec.impl.Main;
+import ru.erminson.ec.impl.repository.RecordBookRepositoryImpl;
+import ru.erminson.ec.model.dto.yaml.YamlRecordBookList;
+import ru.erminson.ec.model.entity.RecordBook;
+import ru.erminson.ec.model.entity.Student;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RecordBookRepositoryInitializer {
+    private static final String RECORD_BOOKS_FILE_NAME = "recordbooks.yaml";
+
+    private RecordBookRepositoryInitializer() {
+        throw new IllegalStateException("RecordBookRepositoryInitializer utility class");
+    }
+
+    public static RecordBookRepository create(StudentRepository studentRepository) {
+        Map<Student, RecordBook> storage = createStorage(studentRepository);
+        return new RecordBookRepositoryImpl(storage);
+    }
+
+    private static Map<Student, RecordBook> createStorage(StudentRepository studentRepository) {
+        YamlRecordBookList list = loadYamlRecordListFromFile();
+        Map<Student, RecordBook> storage = new HashMap<>();
+        list.getRecordBooks().stream()
+                .forEach(recordBookDto -> {
+                    String studentName = recordBookDto.getStudentName();
+                    try {
+                        Student student = studentRepository.getStudentByName(studentName);
+                        RecordBook recordBook = RecordBookInitializer.createRecordBookByCourse(recordBookDto);
+                        storage.put(student, recordBook);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        return storage;
+    }
+
+    private static YamlRecordBookList loadYamlRecordListFromFile() {
+        Yaml yaml = new Yaml(new Constructor(YamlRecordBookList.class));
+        InputStream inputStream = Main
+                .class
+                .getClassLoader()
+                .getResourceAsStream(RECORD_BOOKS_FILE_NAME);
+
+        return yaml.loadAs(inputStream, YamlRecordBookList.class);
+    }
+}
